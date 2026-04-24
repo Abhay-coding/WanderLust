9811d3406f8436d876b573bf88087174bc18d4da
Abhay Kumar <abhay.kumar.8669@gmail.com>
2026-04-24 21:19:01 +0530

    Starting with Authorization

--- app.js
+++ app.js
@@ -64,6 +64,11 @@ app.get("/",(req,res)=>{
     res.send("Working")
 });
 
+app.use((req, res, next) => {
+  res.locals.currUser = req.user;
+  next();
+});
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success"),
     res.locals.error = req.flash("error"),
