03dd5b872095d7d0a3ec10b9132b588f74058760
Abhay Kumar <abhay.kumar.8669@gmail.com>
2026-07-12 14:57:38 +0530

    Edit Listing Image

--- controllers/listings.js
+++ controllers/listings.js
@@ -39,7 +39,7 @@ export const createLisiting = async(req,res,next)=>{
     let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
-    newListing.image = {url, filename};
+    newListing.image = { url, filename };
     await newListing.save();
     req.flash("success","New Listing Created !")
     res.redirect("/listings");
@@ -53,7 +53,16 @@ export const renderEditForm = async (req,res)=>{
 
 export const updateListing = async(req,res)=>{
     let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    let listing = await Listing.findByIdAndUpdate(id,{...req.body.listing});
+
+    if(typeof req.file !== "undefined"){
+        let url =req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
+    
+
     req.flash("success","Listing Updated !")
     res.redirect("/listings")
 };
